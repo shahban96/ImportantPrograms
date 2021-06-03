7813bec3cb68464868b8a8c551f2c920b8bd6d87
Shahban Ali <shahbanalirajput@gmail.com>
2021-06-03 18:59:59 +0530

    Created a project for token generation.

---
+++ GenerateToken/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "shahban@gmail.com";
+            string str1 = str.Split('@').Last();
+            string str2 = str.Split('@')[0];
+            Console.WriteLine(str1 + " " + str2);
+
+            string token = string.Empty;
+            Random rd = new Random();
+            int value = rd.Next(100000);
+            string random = value.ToString("00000");
+            token = str2 + "-" + random;
+            Console.WriteLine(token);
+
+            Console.ReadKey();
+        }
+    }
+}
--- SumOfDigits/Program.cs
+++ SumOfDigits/Program.cs
@@ -10,19 +10,21 @@ namespace SumOfDigits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number to sum the digits");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int quot, rem, sum = 0;
+            //Console.WriteLine("Enter the number to sum the digits");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int quot, rem, sum = 0;
 
-            quot = n;
-            while (quot != 0)
-            {
-                rem = quot % 10;
-                sum = sum + rem;
-                quot = quot / 10;
-            }
+            //quot = n;
+            //while (quot != 0)
+            //{
+            //    rem = quot % 10;
+            //    sum = sum + rem;
+            //    quot = quot / 10;
+            //}
 
-            Console.WriteLine("Sum of digits are {0} ",sum);
+            //Console.WriteLine("Sum of digits are {0} ", sum);
+
+
             Console.ReadLine();
         }
     }
